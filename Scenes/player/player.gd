705688e6ef0b27969617164800e079b84b7f69e7
Vixyv <76610866+Vixyv@users.noreply.github.com>
2023-11-19 22:05:46 -0600

    Progress on player movement

--- Scenes/player/player.gd
+++ Scenes/player/player.gd
@@ -24,3 +24,15 @@ func _physics_process(delta):
 	if direction != Vector3.ZERO:
 		direction = direction.normalized()
 		$Pivot.look_at(position + direction, Vector3.UP)
+	
+	# Ground Velocity
+	target_velocity.x = direction.x * speed
+	target_velocity.z = direction.z * speed
+
+	# Vertical Velocity
+	if not is_on_floor(): # If in the air, fall towards the floor. Literally gravity
+		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
+
+	# Moving the Character
+	velocity = target_velocity
+	move_and_slide()
