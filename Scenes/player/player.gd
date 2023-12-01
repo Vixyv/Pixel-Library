c55e9ff0d62a2cbb1476ebe7d14d8d00c9495597
Vixyv <findlaymc@gmail.com>
2023-12-01 16:18:33 -0600

    Basic 3D character controller finished
    Modified version of "https://github.com/rbarongr/GodotFirstPersonController"

--- Scenes/player/player.gd
+++ Scenes/player/player.gd
@@ -1,24 +1,58 @@
-extends CharacterBody3D
+# Code modified from "https://github.com/rbarongr/GodotFirstPersonController"
 
-const SPEED = 10.0
-const JUMP_VELOCITY = 4.5
+class_name Player extends CharacterBody3D
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+@export_category("Player")
+@export_range(1, 40, 1) var speed: float = 10 # m/s
+@export_range(10, 400, 1) var acceleration: float = 100 # m/s^2
 
-func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y -= gravity * delta
+@export_range(0.1, 3.0, 0.1, "or_greater") var camera_sens: float = 1
 
-	# Get the input direction and handle the movement/deceleration.
-	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+var mouse_captured: bool = false
 
+var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+var move_dir: Vector2 # Input direction for movement
+var look_dir: Vector2 # Input direction for look/aim
+
+var walk_vel: Vector3 # Walking velocity 
+var grav_vel: Vector3 # Gravity velocity 
+
+@onready var camera: Camera3D = $Camera
+
+func _ready() -> void:
+	capture_mouse()
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		look_dir = event.relative * 0.001
+		if mouse_captured: _rotate_camera()
+	if Input.is_action_just_pressed("exit"): get_tree().quit()
+
+func _physics_process(delta: float) -> void:
+	velocity = _walk(delta) + _gravity(delta)
 	move_and_slide()
+
+func capture_mouse() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	mouse_captured = true
+
+func release_mouse() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	mouse_captured = false
+
+func _rotate_camera(sens_mod: float = 1.0) -> void:
+	camera.rotation.y -= look_dir.x * camera_sens * sens_mod
+	camera.rotation.x = clamp(camera.rotation.x - look_dir.y * camera_sens * sens_mod, -1.5, 1.5)
+
+
+func _walk(delta: float) -> Vector3:
+	move_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_backwards")
+	var _forward: Vector3 = camera.global_transform.basis * Vector3(move_dir.x, 0, move_dir.y)
+	var walk_dir: Vector3 = Vector3(_forward.x, 0, _forward.z).normalized()
+	walk_vel = walk_vel.move_toward(walk_dir * speed * move_dir.length(), acceleration * delta)
+	return walk_vel
+
+func _gravity(delta: float) -> Vector3:
+	grav_vel = Vector3.ZERO if is_on_floor() else grav_vel.move_toward(Vector3(0, velocity.y - gravity, 0), gravity * delta)
+	return grav_vel
